d8b1d5eef25cfcfac98d0fa2efcc4fb7b0ea23b3
shineylike <bejanandreea86@gmail.com>
2025-03-24 18:14:41 +0200

    BusinessLogic implementation

---
+++ Shop.BusinessLogic/BLStruct/AuthBL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shop.BusinessLogic.Core;
+using Shop.BusinessLogic.Interface;
+
+namespace Shop.BusinessLogic.BLStruct
+{
+     internal class AuthBL : UserApi, IAuth
+     {
+          public string UserAuthLogic(UserLoginDO data)
+          {
+               return UserAuthLogicAction(data);
+          }
+     }
+}
---
+++ Shop.BusinessLogic/BusinessLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.BusinessLogic
+{
+     public class BusinessLogic
+     {
+          public ISession GetSessionBL()
+          {
+               return new SessionBL();
+          }
+     }
+}
